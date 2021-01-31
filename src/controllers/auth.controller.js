404e47d0030d60c9eef303735299da083212a2f4
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-01-30 20:28:49 -0600

    Guardando para mañana

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,21 +6,25 @@ const User = require('../models/User.model')
 
 //!||
 router.post('/signup', async (req, res, next) => {
-	const { username, email, password } = req.body
-	const user = await User.create({
-		//? Esta es una forma alternativa a el "new user"
-		username,
-		email,
-		password,
-	})
-	//! La encriptacion se hace desde la instancia no desde el modelo
-	user.password = await user.encryptPassword(user.password)
-	await user.save()
-	const token = jwt.sign({ id: user._id }, process.env.SECRET, {
-		expiresIn: 60 * 60 * 24,
-	})
-	res.json({ message: 'The user has been saved', auth: true, token: token })
-	console.log(user)
+	try {
+		const { username, email, password } = req.body
+		const user = await User.create({
+			//? Esta es una forma alternativa a el "new user"
+			username,
+			email,
+			password,
+		})
+		//! La encriptacion se hace desde la instancia no desde el modelo
+		user.password = await user.encryptPassword(user.password)
+		await user.save()
+		const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+			expiresIn: 60 * 60 * 24,
+		})
+		res.json({ message: 'The user has been saved', auth: true, token: token })
+		console.log(user)
+	} catch (err) {
+		console.error(`[Auth controller]: Error ${err}`)
+	}
 })
 
 //!||
@@ -45,11 +49,15 @@ router.get('/me', async (req, res, next) => {
 
 //!||
 router.post('/signin', async (req, res, next) => {
-	const { email, password } = req.body
-	const user = await User.findOne({ email })
-	if (!user) return res.status(404).send('The email does not exist')
-	const passwordIsValid = await user.validatePassword(password)
-	res.json('signin')
+	try {
+		const { email, password } = req.body
+		const user = await User.findOne({ email })
+		if (!user) return res.status(404).send('The email does not exist')
+		const passwordIsValid = await user.validatePassword(password)
+		res.json('signin')
+	} catch (err) {
+		console.error(`[Auth controller]: Error ${err}`)
+	}
 })
 
 module.exports = router
